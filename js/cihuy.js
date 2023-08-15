4d53f74ea7010da5c175a75f4f10b73777f30449
christyuda <yudacihuy8@gmail.com>
2023-08-15 16:18:20 +0700

    Modul Simpelbe: Selesai Perbaikan CSS dan Sidebar

---
+++ js/cihuy.js
@@ -0,0 +1,5 @@
+import { CihuyRootLink } from "https://c-craftjs.github.io/link/link.js";
+import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
+const linknya = CihuyId("link");
+linknya.appendChild(CihuyRootLink);
+CihuyRootLink("./dashboard");
