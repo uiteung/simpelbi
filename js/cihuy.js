9e7ed0d9f7e1eb57b2805ddec669dd2915878d3f
christyuda <chrisyuda@ulbi.ac.id>
2023-08-24 16:07:26 +0700

    Modul SImpelbi Pengecekkan token jika null craftjs

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,5 +1,3 @@
-import { CihuyRootLink } from "https://c-craftjs.github.io/link/link.js";
-import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
-const linknya = CihuyId("link");
-linknya.appendChild(CihuyRootLink);
-CihuyRootLink("./dashboard");
+import { CihuyTokRed } from "https://c-craftjs.github.io/link/link.js";
+
+CihuyTokRed('Login','euis.ulbi.ac.id')
