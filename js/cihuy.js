6b2a06ec465f1c3e2d6f4108e04740af71cebb22
christyuda <chrisyuda@ulbi.ac.id>
2023-08-24 16:17:29 +0700

    Modul Simpelbi: Fix Url

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,5 +1,5 @@
 import { CihuyTokRed } from "https://c-craftjs.github.io/link/link.js";
 
-CihuyTokRed('Login','euis.ulbi.ac.id')
+CihuyTokRed('Login','https://euis.ulbi.ac.id')
 
 console.log(CihuyTokRed);
