cd0eaf0dcf8541f1662274cb856dd84b1eaaabee
christyuda <chrisyuda@ulbi.ac.id>
2023-08-25 10:15:56 +0700

    Modul Simpelbi: Get untuk token, uuid,password

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,6 +1,14 @@
-import { CihuyTokRed } from "https://c-craftjs.github.io/link/link.js";
-
+import {
+  CihuyTokRed,
+  CihuyGetCookie,
+} from "https://c-craftjs.github.io/link/link.js";
 const redirecturl = "https://euis.ulbi.ac.id";
 const token = "login";
 
-CihuyTokRed(token, redirecturl)
+CihuyTokRed(token, redirecturl);
+
+export let token1 = CihuyGetCookie("login");
+export let uuid = CihuyGetCookie("uuid");
+export let password = CihuyGetCookie("password");
+
+console.log(token1, uuid, password);
