e1d8d128bd0f74383aa06b63d1540eaa28eaba83
christyuda <chrisyuda@ulbi.ac.id>
2023-08-30 17:33:34 +0700

    Modul Simpelbi:   pengecekkan admin

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,15 +1,20 @@
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 import { CihuyGetHeaders } from "https://c-craftjs.github.io/api/api.js";
+import { CihuyQuerySelector } from "https://c-craftjs.github.io/element/element.js";
+import { CihuyRole } from "https://c-craftjs.github.io/link/link.js";
 
-let token = CihuyGetCookie("login");
-console.log(token);
+const masukbutton = CihuyQuerySelector(masukButton);
+masukbutton.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let token = CihuyGetCookie("login");
+    const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
+    CihuyGetHeaders(apiUrl, token)
+      .then((result) => {
+        const userRole = JSON.parse(result).role;
+        CihuyRole(userRole);
+      })
+      .catch((error) => console.error("Error:", error));
+  });
+});
 
-// myHeaders.append("LOGIN", token);
-const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
-
-// CihuyGetSimpelbi(target_url, token);
-CihuyGetHeaders(apiUrl, token)
-  .then((result) => console.log(result))
-  .catch((error) => console.error("Error:", error));
-
-console.log(CihuyGetHeaders, ResponseGet);
+console.log(CihuyGetHeaders);
