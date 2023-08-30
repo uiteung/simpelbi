05420c1f6055e477f1c40ab6f70dd91e4e56d48b
christyuda <chrisyuda@ulbi.ac.id>
2023-08-30 16:48:41 +0700

    Modul Simpelbi function get pada admin

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,16 +1,12 @@
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
-import {
-  CihuyGetSimpelbi,
-  ResponseGet,
-} from "https://c-craftjs.github.io/api/api.js";
+import { CihuyGetHeaders } from "https://c-craftjs.github.io/api/api.js";
 
-let token = CihuyGetCookie("login");
+let token = CihuyGetCookie("Login");
 console.log(token);
-let target_url = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
-const responseData = "Data yang ingin Ditampilkan";
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", `Bearer ${token}`);
 
-CihuyGetSimpelbi(target_url, token);
-ResponseGet(responseData);
+myHeaders.append("LOGIN", token);
+const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
+
+// CihuyGetSimpelbi(target_url, token);
+// ResponseGet(responseData);
+CihuyGetHeaders(apiUrl, myHeaders);
