4a54d5fef8b97a4ec5707e103dfca38f0286c072
christyuda <chrisyuda@ulbi.ac.id>
2023-08-30 16:58:39 +0700

    Modul Simpelbi: Menambahkan Token di Get

--- js/cihuy.js
+++ js/cihuy.js
@@ -4,11 +4,11 @@ import { CihuyGetHeaders } from "https://c-craftjs.github.io/api/api.js";
 let token = CihuyGetCookie("Login");
 console.log(token);
 
-myHeaders.append("LOGIN", token);
+// myHeaders.append("LOGIN", token);
 const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
 
 // CihuyGetSimpelbi(target_url, token);
 ResponseGet(responseData);
-CihuyGetHeaders(apiUrl, myHeaders)
+CihuyGetHeaders(apiUrl, token)
   .then((ResponseGet) => console.log(ResponseGet))
   .catch((error) => console.error("Error:", error));
