65d8e9c72037a37a88f7ef6e8646519e54c4b3f2
christyuda <chrisyuda@ulbi.ac.id>
2023-08-30 16:35:47 +0700

    Modul Simpelbi: Menambahkan response dan get

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,34 +1,16 @@
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
-// Definisikan token Anda
-let token = CihuyGetCookie("login"); // Ganti dengan nilai token yang sesuai
+import {
+  CihuyGetSimpelbi,
+  ResponseGet,
+} from "https://c-craftjs.github.io/api/api.js";
 
-// Definisikan myHeaders dengan header LOGIN yang sesuai
-// let myHeaders = new Headers();
-// myHeaders.append("LOGIN", token);
-
-export function customGet(target_url, token, responseFunction) {
-  let myHeaders = new Headers();
-  myHeaders.append("LOGIN", token);
-  let requestOptions = {
-    method: "GET",
-    redirect: "follow",
-    headers: myHeaders,
-  };
-
-  fetch(target_url, requestOptions)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.text();
-    })
-    .then((result) => responseFunction(JSON.parse(result)))
-    .catch((error) => console.error("Error:", error));
-}
-
-export function handleResponse(responseData) {
-  console.log(responseData);
-}
-
+let token = CihuyGetCookie("login");
+console.log(token);
 let target_url = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
-customGet(target_url, token, handleResponse);
+const responseData = "Data yang ingin Ditampilkan";
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", `Bearer ${token}`);
+
+CihuyGetSimpelbi(target_url, token);
+ResponseGet(responseData);
