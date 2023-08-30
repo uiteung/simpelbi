2487e6e73142553baa035db43a4973536ae81c00
christyuda <chrisyuda@ulbi.ac.id>
2023-08-30 17:04:00 +0700

    Modul Simpelbi : Mencoba mengambil dari response

--- js/cihuy.js
+++ js/cihuy.js
@@ -6,9 +6,12 @@ console.log(token);
 
 // myHeaders.append("LOGIN", token);
 const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
+responseData = "Data Berhasil Tampil";
 
 // CihuyGetSimpelbi(target_url, token);
 ResponseGet(responseData);
 CihuyGetHeaders(apiUrl, token)
   .then((ResponseGet) => console.log(ResponseGet))
   .catch((error) => console.error("Error:", error));
+
+console.log(CihuyGetHeaders, ResponseGet);
