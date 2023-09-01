a772156b56ce3e73d73c4efc70047a55da8c1146
christyuda <chrisyuda@ulbi.ac.id>
2023-09-01 16:06:56 +0700

    Modul Simpelbi: Mencoba membuat sidebar

--- app/js/cihuy.js
+++ app/js/cihuy.js
@@ -1,9 +1,7 @@
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 import { CihuyGetHeaders } from "https://c-craftjs.github.io/api/api.js";
 import { CihuyQuerySelector } from "https://c-craftjs.github.io/element/element.js";
-// Fungsi untuk membuat elemen <li> dan <a> sesuai dengan data menu
-// Fungsi untuk membuat elemen <li> dan <a> sesuai dengan data menu
-// Fungsi untuk mendapatkan data menu dari API
+
 function getMenuData() {
   const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/menu/get";
   const token = CihuyGetCookie("login"); // Gantilah dengan token Anda
@@ -20,39 +18,50 @@ function getMenuData() {
 }
 
 // Fungsi untuk membangun sidebar berdasarkan data menu
+// Fungsi untuk membangun sidebar berdasarkan data menu
 function buildSidebar(menuData) {
-  const sidebar = document.getElementById("sidebar"); // Gantilah dengan ID dari elemen sidebar Anda
+  const sidebarNav = document.querySelector(".sidebar_nav"); // Memilih elemen dengan class "sidebar_nav"
 
   menuData.forEach((item) => {
     const menuItem = document.createElement("li");
-    menuItem.classList.add("has-subMenu-left");
 
     if (item.is_main_menu === 0) {
-      // Jika is_main_menu adalah 0, maka buat elemen <a>
+      // Jika is_main_menu adalah 0, maka buat elemen <a> di dalam <li>
       const link = document.createElement("a");
-      link.href = "#";
+      link.href = item.url; // Gantilah dengan URL yang sesuai
       link.innerHTML = `
         <span class="nav-icon ${item.icon}"></span>
         <span class="menu-text">${item.title}</span>
       `;
       menuItem.appendChild(link);
     } else if (item.is_main_menu === 1) {
-      // Jika is_main_menu adalah 1, maka buat elemen <ul> dan <li>
+      // Jika is_main_menu adalah 1, maka buat elemen <li> dengan sub-menu
+      const parentLink = document.createElement("a");
+      parentLink.href = "#";
+      parentLink.classList.add("action");
+
+      parentLink.innerHTML = `
+        <span class="nav-icon ${item.icon}"></span>
+        <span class="menu-text">${item.title}</span>
+        <span class="toggle-icon"></span>
+      `;
+
       const subMenu = document.createElement("ul");
-      subMenu.classList.add("subMenu");
 
-      const subMenuItem = document.createElement("li");
+      item.submenu.forEach((subItem) => {
+        const subMenuItem = document.createElement("li");
+        const subLink = document.createElement("a");
+        subLink.href = subItem.url; // Gantilah dengan URL yang sesuai
+        subLink.innerHTML = subItem.title;
+        subMenuItem.appendChild(subLink);
+        subMenu.appendChild(subMenuItem);
+      });
 
-      const link = document.createElement("a");
-      link.href = item.url; // Gantilah dengan URL yang sesuai
-      link.innerHTML = item.title;
-
-      subMenuItem.appendChild(link);
-      subMenu.appendChild(subMenuItem);
+      menuItem.appendChild(parentLink);
       menuItem.appendChild(subMenu);
     }
 
-    sidebar.appendChild(menuItem);
+    sidebarNav.appendChild(menuItem);
   });
 }
 
