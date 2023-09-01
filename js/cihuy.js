170643724b95e26f7e41ba341ef7ac0b454dd816
christyuda <chrisyuda@ulbi.ac.id>
2023-09-01 15:34:10 +0700

    Modul Simpelbi: Proses Memperbaiki Sidebar

--- js/cihuy.js
+++ js/cihuy.js
@@ -2,32 +2,40 @@ import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 import { CihuyGetHeaders } from "https://c-craftjs.github.io/api/api.js";
 import { CihuyQuerySelector } from "https://c-craftjs.github.io/element/element.js";
 // Fungsi untuk membuat elemen <li> dan <a> sesuai dengan data menu
-function createMenuItem(title, url, icon) {
+// Fungsi untuk membuat elemen <li> dan <a> sesuai dengan data menu
+function createMenuItem(title, url, icon, isMainMenu) {
   const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = url;
-  a.className = "action";
+  if (isMainMenu) {
+    const a = document.createElement("a");
+    a.href = url;
+    a.className = "action";
 
-  if (icon) {
-    const iconSpan = document.createElement("span");
-    iconSpan.className = `nav-icon ${icon}`;
-    a.appendChild(iconSpan);
+    if (icon) {
+      const iconSpan = document.createElement("span");
+      iconSpan.className = `nav-icon ${icon}`;
+      a.appendChild(iconSpan);
+    }
+
+    const textSpan = document.createElement("span");
+    textSpan.className = "menu-text";
+    textSpan.textContent = title;
+
+    a.appendChild(textSpan);
+    li.appendChild(a);
+  } else {
+    const a = document.createElement("a");
+    a.href = url;
+    a.textContent = title;
+    li.appendChild(a);
   }
-
-  const textSpan = document.createElement("span");
-  textSpan.className = "menu-text";
-  textSpan.textContent = title;
-
-  a.appendChild(textSpan);
-  li.appendChild(a);
 
   return li;
 }
 
 // Fungsi untuk mengambil data menu dari API
 function getMenuData() {
+  const token = CihuyGetCookie("login");
   const url = "https://simbe-dev.ulbi.ac.id/api/v1/menu/get";
-  const token = "YOUR_TOKEN_HERE"; // Ganti dengan token yang sesuai
   CihuyGetHeaders(url, token)
     .then((response) => {
       const data = JSON.parse(response);
@@ -35,12 +43,15 @@ function getMenuData() {
         const menuData = data.data;
         const sidebar = document.getElementById("sidebar"); // Ganti 'sidebar' dengan id elemen sidebar Anda
 
+        // Iterasi melalui data menu dan membuat item sidebar
         menuData.forEach((menuItem) => {
           if (menuItem.is_aktif === "y") {
+            const isMainMenu = menuItem.is_main_menu === 1;
             const li = createMenuItem(
               menuItem.title,
               menuItem.url,
-              menuItem.icon
+              menuItem.icon,
+              isMainMenu
             );
             sidebar.appendChild(li);
           }
