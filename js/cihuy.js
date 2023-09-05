ba01417e23c04c533ff5c747c30038c58cacb35f
christyuda <chrisyuda@ulbi.ac.id>
2023-09-05 10:54:04 +0700

    Modul simpelbi: Mengganti isi js

--- js/cihuy.js
+++ js/cihuy.js
@@ -3,67 +3,67 @@ import { CihuyGetHeaders } from "https://c-craftjs.github.io/api/api.js";
 import { CihuyQuerySelector } from "https://c-craftjs.github.io/element/element.js";
 // Fungsi untuk membuat elemen <li> dan <a> sesuai dengan data menu
 // Fungsi untuk membuat elemen <li> dan <a> sesuai dengan data menu
-function createMenuItem(title, url, icon, isMainMenu) {
-  const li = document.createElement("li");
-  if (isMainMenu) {
-    const a = document.createElement("a");
-    a.href = url;
-    a.className = "action";
+const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/menu/get";
+const token = CihuyGetCookie("login");
 
-    if (icon) {
-      const iconSpan = document.createElement("span");
-      iconSpan.className = `nav-icon ${icon}`;
-      a.appendChild(iconSpan);
-    }
+// Fungsi untuk mengambil data dari API
+function fetchDataFromAPI() {
+  const myHeaders = new Headers();
+  myHeaders.append("LOGIN", token);
 
-    const textSpan = document.createElement("span");
-    textSpan.className = "menu-text";
-    textSpan.textContent = title;
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-    a.appendChild(textSpan);
-    li.appendChild(a);
-  } else {
-    const a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
-    li.appendChild(a);
-  }
-
-  return li;
-}
-
-// Fungsi untuk mengambil data menu dari API
-function getMenuData() {
-  const token = CihuyGetCookie("login");
-  const url = "https://simbe-dev.ulbi.ac.id/api/v1/menu/get";
-  CihuyGetHeaders(url, token)
+  return fetch(apiUrl, requestOptions)
     .then((response) => {
-      const data = JSON.parse(response);
-      if (data.success) {
-        const menuData = data.data;
-        const sidebar = document.getElementById("sidebar"); // Ganti 'sidebar' dengan id elemen sidebar Anda
-
-        // Iterasi melalui data menu dan membuat item sidebar
-        menuData.forEach((menuItem) => {
-          if (menuItem.is_aktif === "y") {
-            const isMainMenu = menuItem.is_main_menu === 1;
-            const li = createMenuItem(
-              menuItem.title,
-              menuItem.url,
-              menuItem.icon,
-              isMainMenu
-            );
-            sidebar.appendChild(li);
-          }
-        });
-      } else {
-        console.error("Gagal mengambil data menu");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
+      return response.json(); // Menggunakan response.json() untuk mengambil data JSON dari API
     })
     .catch((error) => {
-      console.error("Terjadi kesalahan:", error);
+      console.error("Error:", error);
+      throw error;
     });
 }
 
-// Panggil fungsi untuk mengambil data menu dan membuat sidebar
-getMenuData();
+// Fungsi untuk mengisi sidebar dengan data dari API
+function populateSidebar(data) {
+  const sidebarNav = document.querySelector(".sidebar_nav");
+
+  // Menghapus semua elemen anak dari sidebarNav
+  while (sidebarNav.firstChild) {
+    sidebarNav.removeChild(sidebarNav.firstChild);
+  }
+
+  // Loop melalui data dari API dan membuat elemen sidebar
+  data.data.forEach((item) => {
+    const listItem = document.createElement("li");
+    if (item.is_main_menu === 1) {
+      // Jika is_main_menu = 1, buat tautan dengan href
+      const link = document.createElement("a");
+      link.href = item.url;
+      link.innerHTML = `<span class="menu-text">${item.title}</span>`;
+      listItem.appendChild(link);
+    } else {
+      // Jika is_main_menu = 0, buat elemen span
+      const span = document.createElement("span");
+      span.className = "menu-text";
+      span.textContent = item.title;
+      listItem.appendChild(span);
+    }
+    sidebarNav.appendChild(listItem);
+  });
+}
+
+// Panggil fungsi fetchDataFromAPI dan populateSidebar
+fetchDataFromAPI()
+  .then((data) => {
+    populateSidebar(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
