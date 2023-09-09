9b6885087cbb709def98f1efb89488fd8fbefa51
christyuda <chrisyuda@ulbi.ac.id>
2023-09-09 12:25:59 +0700

    Modul simpelbi: menambahkan func postwithheaders

--- users/users-admin.js
+++ users/users-admin.js
@@ -1,13 +1,13 @@
-import { CihuyDataAPI } from "https://c-craftjs.github.io/simpelbi/api.js";
+import { CihuyDataAPI ,CihuyPostApi} from "https://c-craftjs.github.io/simpelbi/api.js";
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
-import { CihuyGetHeaders } from "../js/getfunc.js";
+import { Cihuytobase64 } from "https://c-craftjs.github.io/simpelbi/conv.js";
 import { CihuyNameCell } from "./element/namecell.js";
 import { CihuyTableCell } from "./element/tablecell.js";
 import { CihuyAppend } from "./element/append.js";
 
 const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
 const token = CihuyGetCookie("login");
-
+// get data
 CihuyDataAPI(apiUrl, token, (error, data) => {
   const tableBody = document.getElementById("tableBody");
 
@@ -23,7 +23,7 @@ CihuyDataAPI(apiUrl, token, (error, data) => {
         const row = document.createElement("tr");
 
         // Create and populate table cells for each column
-        const idCell = CihuyTableCell("userDatatable-content", item.id);
+        const idCell = CihuyTableCell("userDatatable-content", item.id_admin);
         const namaCell = CihuyNameCell("text-dark fw-500", item.nama);
         const jabatanCell = CihuyTableCell(
           "userDatatable-content",
@@ -46,24 +46,75 @@ CihuyDataAPI(apiUrl, token, (error, data) => {
     }
   }
 });
-// const tableBody = document.getElementById("tableBody"); // Menambahkan ini
 
-// CihuyDataAPI(apiUrl, token, tableBody, (error, data) => {
-//   if (error) {
-//     console.error("Ada masalah saat mengambil data:", error);
-//   } else {
-//     // Inisialisasi AG Grid dan atur data
-//     const gridOptions = {
-//       columnDefs: [
-//         { headerName: "Id", field: "id" },
-//         { headerName: "Nama", field: "nama" },
-//         { headerName: "Jabatan", field: "jabatan" },
-//         { headerName: "Email", field: "email" },
-//         { headerName: "NIDN", field: "nidn" },
-//         { headerName: "Foto", field: "foto" },
-//       ],
-//       rowData: data.data,
-//     };
-//     new agGrid.Grid(document.querySelector("#agGridContainer"), gridOptions);
-//   }
-// });
+
+//postdata
+const tambahDataButton = document.getElementById('tambahDataButton');
+
+tambahDataButton.addEventListener('click', async function (e) {
+  e.preventDefault();
+
+  const namaAdmin = document.getElementById('namaAdmin').value;
+  const jabatan = document.getElementById('jabatan').value;
+  const email = document.getElementById('email').value;
+  const nidn = document.getElementById('nidn').value;
+  const fotoInput = document.getElementById('fotoInput');
+
+  const formData = new FormData();
+  formData.append('nama', namaAdmin);
+  formData.append('jabatan', jabatan);
+  formData.append('email', email);
+  formData.append('nidn', nidn);
+
+  if (fotoInput.files[0]) {
+    const fotoFile = fotoInput.files[0];
+
+    // Mengonversi file gambar menjadi base64
+    const reader = new FileReader();
+    reader.readAsDataURL(fotoFile);
+
+    reader.onload = function () {
+      // Mendapatkan hasil konversi base64
+      const fotoBase64 = reader.result;
+
+      // Menambahkan base64 ke FormData
+      formData.append('foto', fotoBase64);
+
+      // URL endpoint yang sesuai dengan fungsi CihuyPostApi
+      const url = 'https://simbe-dev.ulbi.ac.id/api/v1/admins/add'; // Gantilah dengan URL yang sesuai
+
+      const jsonData = {
+        nama: namaAdmin,
+        jabatan: jabatan,
+        email: email,
+        nidn: nidn,
+        foto: fotoBase64
+    };
+      // Mengirim permintaan POST menggunakan fungsi CihuyPostApi
+      CihuyPostApi(url, token, jsonData)
+        .then(responseText => {
+          console.log('Response:', responseText);
+          window.alert('Data berhasil ditambahkan!');
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          // Tangani kesalahan jika terjadi
+        });
+    };
+  } else {
+    // Jika tidak ada file gambar yang dipilih, Anda masih dapat mengirim data lainnya
+    // URL endpoint yang sesuai dengan fungsi CihuyPostApi
+    const url = 'https://simbe-dev.ulbi.ac.id/api/v1/admins/add'; // Gantilah dengan URL yang sesuai
+
+    // Mengirim permintaan POST menggunakan fungsi CihuyPostApi
+    CihuyPostApi(url, token, formData)
+      .then(responseText => {
+        console.log('Response:', responseText);
+        window.alert('Data berhasil ditambahkan!');
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Tangani kesalahan jika terjadi
+      });
+  }
+});
