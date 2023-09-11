41444a26f7b4ae7ba0fddbbb8df4a45953182993
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-09-11 13:20:17 +0700

    Modul Presensi : Coba Get All AMI

---
+++ ami/ami.js
@@ -0,0 +1,50 @@
+import { CihuyDataAPI, CihuyPostApi } from "https://c-craftjs.github.io/simpelbi/api.js";
+import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
+
+const apiUrl = "http://simbe-dev.ulbi.ac.id/api/v1/ami";
+const token = CihuyGetCookie("login")
+
+// Function Untuk Tampil Data
+function tampilData(data) {
+    const tableBody = document.getElementById("tablebody");
+
+    // Kosongkan isi tabel saat ini
+    tableBody.innerHTML = "";
+    let nomor = 1;
+
+    // Perulangan untuk menampilkan data yang diterima dari API
+    data.forEach((item) => {
+        const barisBaru = document.createElement("tr");
+        barisBaru.innerHTML = `
+            <td>${nomor}</td>
+            <td>${item.id_fakultas}</td>
+            <td>${item.id_prodi}</td>
+            <td>${item.id_auditor_ketua}</td>
+            <td>${item.id_anggota1}</td>
+            <td>${item.id_anggota2}</td>
+            <td>${item.id_siklus}</td>
+            <td>${item.status}</td>
+            <td>${item.tgl_rtm}</td>
+            <td>${item.tgl_selesai}</td>
+            <td>
+            <ul class="orderDatatable_actions mb-0 d-flex flex-wrap">
+                <li>
+                <a href="#" class="view">
+                    <i class="uil uil-eye"></i>
+                </a>
+                </li>
+                <li>
+                <a href="#" class="edit">
+                    <i class="uil uil-edit"></i>
+                </a>
+                </li>
+                <li>
+                <a href="#" class="remove">
+                    <i class="uil uil-trash-alt"></i>
+                </a>
+                </li>
+            </ul>
+            </td>
+        `;
+    })
+}
