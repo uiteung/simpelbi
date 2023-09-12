47d90324fdd5d359cd8881a55fe4a9962a443155
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-09-12 10:26:33 +0700

    Modul Presensi : Coba GET AMI

--- ami/ami.js
+++ ami/ami.js
@@ -1,63 +1,42 @@
-import { CihuyDataAPI, CihuyPostApi } from "https://c-craftjs.github.io/simpelbi/api.js";
-import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
-
-const apiUrl = "http://simbe-dev.ulbi.ac.id/api/v1/ami";
-const token = CihuyGetCookie("login")
-
-// Function Untuk Tampil Data
-function tampilData(data) {
-    const tableBody = document.getElementById("tablebody");
-
-    // Kosongkan isi tabel saat ini
-    tableBody.innerHTML = "";
-    let nomor = 1;
-
-    // Perulangan untuk menampilkan data yang diterima dari API
-    data.forEach((item) => {
-        const barisBaru = document.createElement("tr");
-        barisBaru.innerHTML = `
-            <td>${nomor}</td>
-            <td>${item.id_fakultas}</td>
-            <td>${item.id_prodi}</td>
-            <td>${item.id_auditor_ketua}</td>
-            <td>${item.id_anggota1}</td>
-            <td>${item.id_anggota2}</td>
-            <td>${item.id_siklus}</td>
-            <td>${item.status}</td>
-            <td>${item.tgl_rtm}</td>
-            <td>${item.tgl_selesai}</td>
-            <td>
-            <ul class="orderDatatable_actions mb-0 d-flex flex-wrap">
-                <li>
-                <a href="#" class="view">
-                    <i class="uil uil-eye"></i>
-                </a>
-                </li>
-                <li>
-                <a href="#" class="edit">
-                    <i class="uil uil-edit"></i>
-                </a>
-                </li>
-                <li>
-                <a href="#" class="remove">
-                    <i class="uil uil-trash-alt"></i>
-                </a>
-                </li>
-            </ul>
-            </td>
-        `;
-        tableBody.appendChild(barisBaru);
-        nomor++;
-    });
-}
-
-// Get Data dari API
-CihuyDataAPI(apiUrl, token, (error, response) => {
+import { CihuyPostAMI, ShowDataAMI } from "../js/config/configami.js"
+import { UrlAMI, token, UrlPostAMI } from "../js/template/template.js";
+import {
+    CihuyDataAPI,
+  } from "https://c-craftjs.github.io/simpelbi/api.js";
+  
+CihuyDataAPI(UrlAMI, token, (error, response) => {
     if (error) {
-        console.error("Terjadi Kesalahan : ", error);
+      console.error("Terjadi kesalahan:", error);
     } else {
-        const data = response.data;
-        console.log("Data yang diterima : ", data);
-        tampilData(data);
+      const data = response.data;
+    //   console.log("Data yang diterima:", data);
+      ShowDataAMI(data);
     }
+  });
+
+const Tombol = document.getElementById("buttoninsert");
+
+Tombol.addEventListener("click", async function (e) {
+  e.preventDefault();
+  console.log("Button Clicked");
+
+  const amival = document.getElementById("ami").value;
+  
+  var raw = JSON.stringify({
+    "ami": amival
+  });
+
+  // Mengirim permintaan POST menggunakan fungsi CihuyPostApi
+  CihuyPostAMI(UrlPostAMI, token, raw)
+  .then((responseText) => {
+    console.log("Response:", responseText);
+    window.alert("Data berhasil ditambahkan!");
+    window.location.replace("https://euis.ulbi.ac.id/simpelbi/ami/ami.html")
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+    // Tangani kesalahan jika terjadi
+  });
+
 });
+
