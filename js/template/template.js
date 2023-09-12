237d16c66424345c2222c8c34c13567c440758cd
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-09-12 10:49:50 +0700

    Modul Simpelbi : rapihin code users admin

--- js/template/template.js
+++ js/template/template.js
@@ -1,11 +1,17 @@
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
-export let Url = "https://simbe-dev.ulbi.ac.id/api/v1/siklus"
-export let UrlKts = "https://simbe-dev.ulbi.ac.id/api/v1/kts"
-export let urlPostKts = "https://simbe-dev.ulbi.ac.id/api/v1/kts/add";
-export let urlPostSiklus = "https://simbe-dev.ulbi.ac.id/api/v1/siklus/add"
-export let UrlGetJenjang = "https://simbe-dev.ulbi.ac.id/api/v1/jenjang"
-export let UrlPostJenjang = "https://simbe-dev.ulbi.ac.id/api/v1/jenjang/add"
+
+// Endpoint GET
+export let Url = "https://simbe-dev.ulbi.ac.id/api/v1/siklus";
+export let UrlKts = "https://simbe-dev.ulbi.ac.id/api/v1/kts";
+export let UrlGetJenjang = "https://simbe-dev.ulbi.ac.id/api/v1/jenjang";
 export let UrlGetStandar = "https://simbe-dev.ulbi.ac.id/api/v1/standar";
+export let UrlGetUsersAdmin = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
+
+// Endpoint POST
+export let urlPostKts = "https://simbe-dev.ulbi.ac.id/api/v1/kts/add";
+export let urlPostSiklus = "https://simbe-dev.ulbi.ac.id/api/v1/siklus/add";
+export let UrlPostJenjang = "https://simbe-dev.ulbi.ac.id/api/v1/jenjang/add";
+
 // export let token = "v4.public.eyJleHAiOiIyMDIzLTA5LTEyVDExOjUxOjQwKzA3OjAwIiwiaWF0IjoiMjAyMy0wOS0xMlQwOTo1MTo0MCswNzowMCIsImlkIjoiNjI4NTE1NjAwNzEzNyIsIm5iZiI6IjIwMjMtMDktMTJUMDk6NTE6NDArMDc6MDAifYKwrCmgvEbYNm0Kxcj3KQr0tHHhkzlf9OdP2FbEZVgSR0kw2YRUSccVQCjz4EqSZmq3Q6qIT8dMwGH7kLymPQA"
 export let token = CihuyGetCookie("login")
 export const tambahDataButton = document.getElementById("simpanbutt");
