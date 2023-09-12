b0c6f358ee7abe8f090c21bc4fc267732cb726b5
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-09-12 11:17:32 +0700

    change template + users audit fakultas prodi

---
+++ js/config/configusersprodi.js
@@ -0,0 +1,82 @@
+export function ShowDataUsersProdi(data) {
+    const tableBody = document.getElementById("content");
+  
+    // Kosongkan isi tabel saat ini
+    tableBody.innerHTML = "";
+    let nomor = 1;
+  
+    // Loop melalui data yang diterima dari API
+    data.forEach((item) => {
+      const barisBaru = document.createElement("tr");
+      barisBaru.innerHTML = `
+      <td>
+         <div class="userDatatable-content">${nomor}</div>
+      </td>
+      <td>
+         <div class="d-flex">
+            <div class="userDatatable-inline-title">
+               <a href="#" class="text-dark fw-500">
+                  <h6>${item.idFakultas}</h6>
+               </a>
+            </div>
+         </div>
+      </td>
+      <td>
+         <div class="userDatatable-content">
+            ${item.prodi}
+         </div>
+      </td>
+      <td>
+         <div class="userDatatable-content">
+            ${item.jenjang}
+         </div>
+      </td>
+      <td>
+         <div class="userDatatable-content">
+            ${item.kaprodi}
+         </div>
+      </td>
+      <td>
+         <div class="userDatatable-content">
+            ${item.nidn}
+         </div>
+      </td>
+      <td>
+         <div class="userDatatable-content">
+            ${item.niknip}
+         </div>
+      </td>
+      <td>
+         <div class="userDatatable-content">
+            ${item.telp}
+         </div>
+      </td>
+      <td>
+        <div class="userDatatable-content">
+            ${item.email}
+        </div>
+      </td>
+      <td>
+         <ul class="orderDatatable_actions mb-0 d-flex flex-wrap">
+            <li>
+               <a href="#" class="view">
+                  <i class="uil uil-eye"></i>
+               </a>
+            </li>
+            <li>
+               <a href="#" class="edit">
+                  <i class="uil uil-edit"></i>
+               </a>
+            </li>
+            <li>
+               <a href="#" class="remove">
+                  <i class="uil uil-trash-alt"></i>
+               </a>
+            </li>
+         </ul>
+      </td>
+      `;
+      tableBody.appendChild(barisBaru);
+      nomor++;
+    });
+  }
--- js/template/template.js
+++ js/template/template.js
@@ -1,16 +1,23 @@
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
+
+// Endpoint GET
 export let Url = "https://simbe-dev.ulbi.ac.id/api/v1/siklus";
 export let UrlKts = "https://simbe-dev.ulbi.ac.id/api/v1/kts";
 export let UrlGetJenjang = "https://simbe-dev.ulbi.ac.id/api/v1/jenjang";
 export let UrlGetStandar = "https://simbe-dev.ulbi.ac.id/api/v1/standar";
-export let UrlPostStandar = "https://simbe-dev.ulbi.ac.id/api/v1/standar/add"
-// export let token = "v4.public.eyJleHAiOiIyMDIzLTA5LTEyVDEyOjQzOjI1KzA3OjAwIiwiaWF0IjoiMjAyMy0wOS0xMlQxMDo0MzoyNSswNzowMCIsImlkIjoiNjI4NTE1NjAwNzEzNyIsIm5iZiI6IjIwMjMtMDktMTJUMTA6NDM6MjUrMDc6MDAifRq3smBKnnIws4lmWjrg4lUtgPrCDcKDw-8qmHr1PAZcrRRz29DBDQgFdOr-6lMbn0muOOPbWa2LmIs6vqw_dwc"
+export let UrlPostStandar = "https://simbe-dev.ulbi.ac.id/api/v1/standar/add";
 export let UrlGetUsersAdmin = "https://simbe-dev.ulbi.ac.id/api/v1/admins/";
+export let UrlGetUsersAuditor = "https://simbe-dev.ulbi.ac.id/api/v1/auditors/";
+export let UrlGetUsersFakultas = "https://simbe-dev.ulbi.ac.id/api/v1/fakultas/";
+export let UrlGetUsersProdi = "https://simbe-dev.ulbi.ac.id/api/v1/prodi/";
+export let UrlAMI = "https://simbe-dev.ulbi.ac.id/api/v1/ami"
+
 // Endpoint POST
 export let urlPostKts = "https://simbe-dev.ulbi.ac.id/api/v1/kts/add";
 export let urlPostSiklus = "https://simbe-dev.ulbi.ac.id/api/v1/siklus/add";
 export let UrlPostJenjang = "https://simbe-dev.ulbi.ac.id/api/v1/jenjang/add";
 
-export let UrlAMI = "https://simbe-dev.ulbi.ac.id/api/v1/ami"
 export let token = CihuyGetCookie("login")
 export const tambahDataButton = document.getElementById("simpanbutt");
+
+// export let token = "v4.public.eyJleHAiOiIyMDIzLTA5LTEyVDEyOjQzOjI1KzA3OjAwIiwiaWF0IjoiMjAyMy0wOS0xMlQxMDo0MzoyNSswNzowMCIsImlkIjoiNjI4NTE1NjAwNzEzNyIsIm5iZiI6IjIwMjMtMDktMTJUMTA6NDM6MjUrMDc6MDAifRq3smBKnnIws4lmWjrg4lUtgPrCDcKDw-8qmHr1PAZcrRRz29DBDQgFdOr-6lMbn0muOOPbWa2LmIs6vqw_dwc"
