c8063d7cf0298dfd455496bb7b38311840e084bc
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-09-18 15:41:43 +0700

    Modul Presensi : Coba Post Standar lagi

--- ami/standar.js
+++ ami/standar.js
@@ -14,24 +14,6 @@ CihuyDataAPI(UrlGetStandar, token, (error, response) => {
     }
   });
 
-// const Tombol = document.getElementById("buttonadd");
-
-// Tombol.addEventListener("click", async function (e) {
-//   e.preventDefault();
-//   console.log("Button Clicked");
-
-//   const standarval = document.getElementById("standar").value;
-//   const pilihanval = document.getElementById("utkpilihan").value;
-//   const isival = document.getElementById("isi").value;
-  
-//   var raw = JSON.stringify({
-//     "standar": standarval,
-//     "utkPilihan": pilihanval,
-//     "isi": isival,
-//     "idSiklus": 1
-//   });
-// });
-
 // Untuk POST Data dengan menggunakan API
 function siklusdata(data) {
   const selectElement = document.getElementById("siklus");
@@ -51,62 +33,6 @@ function siklusdata(data) {
   });
 }
 
-// Untuk Ambil nilai dari elemen
-const standarInput = document.getElementById("standar").value;
-const untukPilihan = document.getElementById("utkpilihan").value;
-const isiInput = document.getElementById("isi").value;
-const siklusInput = document.getElementById("siklus").value;
-
-// Menambahkan event listener ke tombol simpan
-document
-  .getElementById("buttonadd")
-  .addEventListener("click", async function () {
-      // Menambahkan nilai dari elemen formulir
-      const Standar = standarInput.value;
-      const Pilihan = untukPilihan.value;
-      const Isi = isiInput.value;
-      const Siklus = siklusInput.value;
-
-      // Cek apakah ada field yang kosong pada form
-      if (!Standar || !Pilihan || !Isi || !Siklus) {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Harap isi semua bidang formulir!"
-        });
-        return;
-      }
-
-      reader.onload = async function () {
-        const data = {
-          Standar: Standar,
-          Pilihan: Pilihan,
-          Isi: Isi,
-          Siklus: parseInt(Siklus),
-        };
-
-        try {
-          // Mengirim permintaan POST ke server menggunakan fungsi CihuyPostApi
-          await CihuyPostApi(UrlPostStandar, token, data);
-
-          // Sembunyikan modal setelah berhasil
-          document.getElementById("new-member").style.display = "none";
-
-          // Tampilkan SweetAlert
-          window.location.reload();
-        } catch (error) {
-          console.error("Terjadi kesalahan: ", error);
-          console.log("Data yang dikirimkan: ", data);
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Terjadi kesalahan saat menyimpan data.",
-          });
-          // Handle kesalahan jika terjadi
-        }
-      };
-})
-
 // Panggil API untuk mendapatkan data siklus
 const siklusapi = "https://simbe-dev.ulbi.ac.id/api/v1/siklus/";
 
@@ -120,14 +46,36 @@ CihuyDataAPI(siklusapi, token, (error, response) => {
   }
 });
 
-// // Mengirim permintaan POST menggunakan fungsi CihuyPostApi
-// CihuyPostKTS(UrlPostStandar, token, raw)
-//   .then((responseText) => {
-//     console.log("Response:", responseText);
-//     window.alert("Data berhasil ditambahkan!");
-//     window.location.replace("https://euis.ulbi.ac.id/simpelbi/ami/standar.html")
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error);
-//     // Tangani kesalahan jika terjadi
-//   });
+const Tombol = document.getElementById("buttonadd");
+
+Tombol.addEventListener("click", async function (e) {
+  e.preventDefault();
+  console.log("Button Clicked");
+
+  // Untuk Ambil nilai dari elemen
+  const standarInput = document.getElementById("standar").value;
+  const untukPilihan = document.getElementById("utkpilihan").value;
+  const isiInput = document.getElementById("isi").value;
+  const siklusInput = document.getElementById("siklus").value;
+
+  var raw = JSON.stringify({
+    "standar" : standarInput,
+    "utkPilihan": untukPilihan,
+    "isi": isiInput,
+    "idSiklus": siklusInput
+  });
+
+  // Mengirimkan Permintaan POST Menggunakan Fungsi CihuyPostApi
+  CihuyPostApi(UrlPostStandar, token, raw)
+  .then((responseText) => {
+    console.log("Response :", responseText);
+    window.alert("Data Berhasil ditambahkan!");
+    window.location.replace("https://euis.ulbi.ac.id/simpelbi/ami/standar.html");
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
+    //  Tangani kesalahan jika terjadi
+  });
+
+});
+
