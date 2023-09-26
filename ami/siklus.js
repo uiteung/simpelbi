f10ae1f9301c4577fdd6fe58b18f379924499492
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-09-26 14:15:00 +0700

    Modul Presensi : Coba Delete Siklus

--- ami/siklus.js
+++ ami/siklus.js
@@ -1,9 +1,7 @@
 // import { tampilData } from "../js/config/configsiklus.js";
 import { CihuyPostKTS } from "../js/config/configkts.js"
 import { UrlGetSiklus, token, UrlPostSiklus } from "../js/template/template.js";
-import {
-    CihuyDataAPI, CihuyDeleteAPI,
-  } from "https://c-craftjs.github.io/simpelbi/api.js";
+import { CihuyDataAPI, CihuyDeleteAPI } from "https://c-craftjs.github.io/simpelbi/api.js";
 
 // Untuk Get Data dari API
 export function tampilData(data) {
@@ -18,36 +16,36 @@ export function tampilData(data) {
     const barisBaru = document.createElement("tr");
     barisBaru.innerHTML = `
     <td>
-    <div class="userDatatable-content">${nomor}</div>
- </td>
- <td>
-    <div class="d-flex">
-       <div class="userDatatable-inline-title">
-          <a href="#" class="text-dark fw-500">
-             <h6>${item.tahun}</h6>
-          </a>
-       </div>
-    </div>
- </td>
- <td>
-    <ul class="orderDatatable_actions mb-0 d-flex flex-wrap">
-       <li>
-          <a href="#" class="view">
-             <i class="uil uil-eye"></i>
-          </a>
-       </li>
-       <li>
-          <a href="#" class="edit">
-             <i class="uil uil-edit"></i>
-          </a>
-       </li>
-       <li>
-          <a href="#" class="remove" data-siklus-id=${item.idSiklus}>
-             <i class="uil uil-trash-alt"></i>
-          </a>
-       </li>
-    </ul>
- </td>
+        <div class="userDatatable-content">${nomor}</div>
+    </td>
+    <td>
+        <div class="d-flex">
+          <div class="userDatatable-inline-title">
+              <a href="#" class="text-dark fw-500">
+                <h6>${item.tahun}</h6>
+              </a>
+          </div>
+        </div>
+    </td>
+    <td>
+        <ul class="orderDatatable_actions mb-0 d-flex flex-wrap">
+          <li>
+              <a href="#" class="view">
+                <i class="uil uil-eye"></i>
+              </a>
+          </li>
+          <li>
+              <a href="#" class="edit">
+                <i class="uil uil-edit"></i>
+              </a>
+          </li>
+          <li>
+              <a href="#" class="remove" data-siklus-id=${item.idSiklus}>
+                <i class="uil uil-trash-alt"></i>
+              </a>
+          </li>
+        </ul>
+    </td>
       `;
     const removeButton = barisBaru.querySelector(".remove");
     removeButton.addEventListener("click", () => {
@@ -68,29 +66,32 @@ CihuyDataAPI(UrlGetSiklus, token, (error, response) => {
       console.error("Terjadi kesalahan:", error);
     } else {
       const data = response.data;
-    //   console.log("Data yang diterima:", data);
+      console.log("Data yang diterima:", data);
       tampilData(data);
     }
   });
 
+// Untuk POST Data
 const Tombol = document.getElementById("simpanbutt");
-
 Tombol.addEventListener("click", async function (e) {
   e.preventDefault();
   console.log("Button Clicked");
-
   const thnval = document.getElementById("thn").value;
-  
   var raw = JSON.stringify({
     "tahun": thnval
   });
-
   // Mengirim permintaan POST menggunakan fungsi CihuyPostApi
   CihuyPostKTS(UrlPostSiklus, token, raw)
   .then((responseText) => {
     console.log("Response:", responseText);
-    window.alert("Data berhasil ditambahkan!");
-    window.location.replace("https://euis.ulbi.ac.id/simpelbi/ami/siklus.html")
+    Swal.fire({
+      icon: "success",
+      title: "Sukses!",
+      text: "Data siklus berhasil ditambahkan.",
+    }).then(() => {
+      // Refresh halaman setelah menutup popup
+      window.location.reload();
+    });
   })
   .catch((error) => {
     console.error("Error:", error);
