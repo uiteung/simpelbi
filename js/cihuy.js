df1e66ae85ee8462c287b1123d18537abfe074b7
christyuda <chrisyuda@ulbi.ac.id>
2023-10-02 10:45:07 +0700

    Modul Simpelbi: Mencoba Cek url user

--- js/cihuy.js
+++ js/cihuy.js
@@ -80,3 +80,53 @@ CihuyGetHeaders(apiUrl, token)
   .catch((error) => {
     console.error("Error:", error);
   });
+
+// Fungsi untuk memeriksa URL sesuai dengan peran atau mengarahkan ke URL yang sesuai
+function checkRoleAndURL(apiUrl, token) {
+  CihuyDataAPI(apiUrl, token, (error, data) => {
+    if (error) {
+      console.error("Gagal mengambil data menu:", error);
+      return;
+    }
+
+    if (data.success) {
+      const role = data.data ? data.data.replace("/", "") : null; // Menghapus karakter '/' dari data respon jika ada
+      const currentUrl = window.location.href;
+
+      if (role === "admin") {
+        // Jika peran adalah 'admin', arahkan pengguna ke URL admin
+        window.location.href =
+          "https://euis.ulbi.ac.id/simpelbi/admins/dashboard.html";
+      } else {
+        // Pengecualian untuk peran 'admin'
+        if (
+          currentUrl ===
+          `https://euis.ulbi.ac.id/simpelbi/${role}/dashboard-${role}.html`
+        ) {
+          console.log("URL sesuai dengan peran.");
+        } else {
+          console.log(
+            "URL tidak sesuai dengan peran. Mengarahkan ke URL yang sesuai..."
+          );
+
+          if (role) {
+            // Jika peran ada, arahkan pengguna ke URL yang sesuai dengan peran
+            window.location.href = `https://euis.ulbi.ac.id/simpelbi/${role}/dashboard-${role}.html`;
+          } else {
+            console.error("Tidak ada peran yang ditemukan dalam data.");
+          }
+        }
+      }
+    } else {
+      if (data.status === "Forbidden") {
+        console.error("Status Forbidden: Pengguna tidak diizinkan.");
+        // Handle akses yang tidak diizinkan di sini, misalnya, menampilkan pesan kesalahan.
+      } else {
+        console.error("Gagal mengambil data menu: Data tidak valid.");
+      }
+    }
+  });
+}
+// Panggil fungsi checkRoleAndURL untuk memeriksa URL sesuai dengan peran atau mengarahkan jika diperlukan
+const apiUrlMenu = "https://simbe-dev.ulbi.ac.id/api/v1/menu/";
+checkRoleAndURL(apiUrlMenu, token);
