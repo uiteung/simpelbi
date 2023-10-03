96d90b3b2679462f665698bda90cdf1f15e5b53f
christyuda <chrisyuda@ulbi.ac.id>
2023-10-03 10:44:13 +0700

    Modul Simpelbi : Penambahan Prodi dan files prodi

--- users/fakultas/add.js
+++
@@ -1,3 +0,0 @@
-import { CihuyDataAPI } from "https://c-craftjs.github.io/simpelbi/api.js";
-import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
-
--- users/users-prodi.js
+++ users/users-prodi.js
@@ -69,22 +69,97 @@ function ShowDataUsersProdi(data) {
                </a>
             </li>
             <li>
-               <a href="#" class="edit">
-                  <i class="uil uil-edit"></i>
+            <a href="#" class="edit"  data-target="#new-member-update" data-prodi-id="${item.id_prodi}">
+            <i class="uil uil-edit"></i>
                </a>
             </li>
             <li>
-               <a href="#" class="remove">
+            <a href="#" class="remove" data-prodi-id="${item.id_prodi}">
                   <i class="uil uil-trash-alt"></i>
                </a>
             </li>
          </ul>
       </td>
       `;
+    const removeButton = barisBaru.querySelector(".remove");
+    removeButton.addEventListener("click", () => {
+      const id_prodi = removeButton.getAttribute("data-prodi-id");
+      if (id_prodi) {
+        deletefakultas(id_prodi);
+      } else {
+        console.error("ID prodi tidak ditemukan.");
+      }
+    });
+    const editButton = barisBaru.querySelector(".edit");
+    editButton.addEventListener("click", () => {
+      const id_prodi = editButton.getAttribute("data-prodi-id");
+      if (id_prodi) {
+        editData(id_prodi);
+      } else {
+        console.error("ID prodi tidak ditemukan.");
+      }
+    });
     tableBody.appendChild(barisBaru);
     nomor++;
   });
 }
+function editData(id_prodi) {
+  // Gunakan CihuyDataAPI untuk mengambil data dari server
+  CihuyDataAPI(
+    UrlGetUsersProdi + `get?idprodi=${id_prodi}`,
+    token,
+    (error, response) => {
+      if (error) {
+        console.error("Terjadi kesalahan:", error);
+      } else {
+        const data = response.data;
+        console.log("Data yang diterima:", data);
+
+        // Isi input dalam modal dengan data yang diterima dari server
+        // document.getElementById("fakultas-update").value = data.prodi;
+        // document.getElementById("dekan-update").value = data.prodi;
+        // document.getElementById("nidn-update").value = data.prodi;
+        // document.getElementById("niknip-update").value = data.prodi;
+        // document.getElementById("telp-update").value = data.prodi;
+        // document.getElementById("email-update").value = data.prodi;
+
+        // Tampilkan modal
+        const modal = new bootstrap.Modal(
+          document.getElementById("new-member-update")
+        );
+        modal.show();
+      }
+    }
+  );
+}
+
+function populateDropdown() {
+  const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/fakultas/";
+  const dropdown = document.getElementById("fakultasDropdown-update");
+
+  // Call the CihuyDataAPI function to fetch the Fakultas data
+  CihuyDataAPI(apiUrl, token, (error, response) => {
+    if (error) {
+      console.error("Terjadi kesalahan:", error);
+    } else {
+      const data = response.data;
+      console.log("Data Fakultas yang diterima:", data);
+
+      // Clear the dropdown options
+      dropdown.innerHTML = "";
+
+      // Create and append option elements to the dropdown
+      data.forEach((item) => {
+        const option = document.createElement("option");
+        option.value = item.fakultas;
+        option.textContent = item.fakultas;
+        dropdown.appendChild(option);
+      });
+    }
+  });
+}
+
+populateDropdown();
 
 CihuyDataAPI(UrlGetUsersProdi, token, (error, response) => {
   if (error) {
