6bb51dc9eaff7e7b2fd9ab98c0e47ee27356b6f5
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-10-10 12:06:17 +0700

    Modul Simpelbi : Coba Update AMI

--- ami/ami.js
+++ ami/ami.js
@@ -12,6 +12,7 @@ import {
   CihuyDataAPI,
   CihuyPostApi,
   CihuyDeleteAPI,
+  CihuyUpdateApi,
 } from "https://c-craftjs.github.io/simpelbi/api.js";
 
 // Untuk GET All Data
@@ -86,7 +87,7 @@ export function ShowDataAMI(data) {
              </a>
           </li>
           <li>
-             <a href="#" class="edit">
+             <a href="#" class="edit" data-target="#new-member-update" data-ami-id="${item.idAmi}">
                 <i class="uil uil-edit"></i>
              </a>
           </li>
@@ -98,6 +99,7 @@ export function ShowDataAMI(data) {
        </ul>
     </td>
       `;
+    // Untuk Remove Button
     const removeButton = barisBaru.querySelector(".remove");
     removeButton.addEventListener("click", () => {
       console.log("Tombol remove diklik");
@@ -108,6 +110,17 @@ export function ShowDataAMI(data) {
         console.error("ID AMI tidak ditemukan");
       }
     });
+    // Untuk Update Button
+    const editButton = barisBaru.querySelector(".edit");
+    editButton.addEventListener("click", () => {
+      console.log("Tombol remove diklik");
+      const amiId = editButton.getAttribute("data-ami-id");
+      if (amiId) {
+        editData(amiId);
+      } else {
+        console.error("ID AMI tidak ditemukan")
+      }
+    })
     tableBody.appendChild(barisBaru);
     nomor++;
   });
@@ -440,4 +453,288 @@ Tombol.addEventListener("click", async function (e) {
         });
     }
   });
+});
+
+// Untuk PUT Data dengan menggunakan API
+function getAmiDataById(idAmi, callback) {
+  const UrlGetAmiById = `https://simbe-dev.ulbi.ac.id/api/v1/ami/get?idami=${idAmi}`;
+
+  CihuyDataAPI(UrlGetAmiById, token, (error, response) => {
+    if (error) {
+      console.error("Terjadi kesalahan saat mengambil data : ", error);
+      callback(error, null);
+    } else {
+      const amiData = response.data;
+      callback(null, amiData);
+    }
+  })
+}
+
+function editData(idAmi) {
+  getAmiDataById(idAmi, (error, amiData) => {
+    if (error) {
+      console.error("Gagal mengambil data AMI : ", error);
+      return;
+    }
+    // Untuk ambil nilai dari form
+    document.getElementById("fakultas-update").value = amiData.id_fakultas;
+    document.getElementById("prodi-update").value = amiData.id_prodi;
+    document.getElementById("auditor-update").value = amiData.idAuditorKetua;
+    document.getElementById("anggota1-update").value = amiData.idAnggota1;
+    document.getElementById("anggota2-update").value = amiData.idAnggota2;
+    document.getElementById("siklus-update").value = amiData.idSiklus;
+
+    // Menampilkan modal edit
+    const modal = new bootstrap.Modal(
+      document.getElementById("new-member-update")
+    );
+    modal.show();
+
+    // Membuat event listener untuk button update
+    const simpanPerubahanButton = document.getElementById("updateDataButton");
+    simpanPerubahanButton.addEventListener("click", function () {
+      // Untuk ambil nilai dari element form edit
+      const fakultasBaru = document.getElementById("fakultas-update").value;
+      const prodiBaru = document.getElementById("prodi-update").value;
+      const auditorBaru = document.getElementById("auditor-update").value;
+      const anggota1Baru = document.getElementById("anggota1-update").value;
+      const anggota2Baru = document.getElementById("anggota2-update").value;
+      const siklusBaru = document.getElementById("siklus-update").value;
+
+      // Buat const untuk nampung semuanya
+      const dataAmiToUpdate = {
+        fakultas: fakultasBaru,
+        prodi: prodiBaru,
+        nm_auditor_ketua: auditorBaru,
+        nm_auditor_1: anggota1Baru,
+        nm_auditor_2: anggota2Baru,
+        siklus: siklusBaru,
+      }
+
+      // Hide modal ketika sudah selesai isi
+      $('#new-member-update').modal('hide');
+
+      // Tampilkan SweetAlet konfirmasi sebelum mengirim permintaan
+      Swal.fire({
+        title: "Update Proses AMI?",
+        text: "Apakah Anda yakin ingin update Proses AMI?",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: "Ya, Update",
+        cancelButtonText: "Bata;",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          sendUpdateAmi(idAmi, dataAmiToUpdate, modal);
+        }
+      })
+    })
+  })
+}
+// function untuk kirim update data
+function sendUpdateAmi(idAmi, dataAmiToUpdate, modal) {
+  const UrlPutAmi = `https://simbe-dev.ulbi.ac.id/api/v1/ami/update?idami=${idAmi}`;
+
+  CihuyUpdateApi(UrlPutAmi, token, dataAmiToUpdate, (error, responseText) => {
+    if (error) {
+      console.error("Terjadi kesalahan saat update data AMI : ", error);
+      Swal.fire({
+        icon: "error", 
+        title: "Oops...",
+        text: "Terjadi kesalahan saat update data AMI",
+      });
+    } else {
+      console.log("Respon sukses :", responseText);
+      // Tutup modal
+      modal.hide()
+      // Tampilkan Sweet Alert sukses
+      Swal.fire({
+        icon: "success",
+        title: "Sukses!",
+        text: "Data Proses AMI berhasil diperbarui",
+        showConfirmButton: false,
+        timer: 1500
+      }).then(() => {
+        window.location.reload();
+      })
+    }
+  })
+}
+// Untuk ambil nilai dari FAKULTAS ke dropdown
+function fakultasDataUpdate(data) {
+  const selectElement = document.getElementById("fakultas-update");
+  // Kosongkan Isi dropdown saat ini
+  selectElement.innerHTML = "";
+
+  // Loop data yang diterima dari API
+  data.forEach((item) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = item.id_fakultas;
+    optionElement.textContent = `${item.fakultas} - ${item.id_fakultas}`;
+    selectElement.appendChild(optionElement);
+  });
+  selectElement.addEventListener("change", function () {
+    const selectedValue = this.value;
+    // Lakukan sesuatu dengan nilai yang dipilih, misalnya, tampilkan di konsol
+    console.log("Nilai yang dipilih: ", selectedValue);
+  });
+}
+// Panggil API untuk mendapatkan data fakultas
+CihuyDataAPI(UrlGetUsersFakultas, token, (error, response) => {
+  if (error) {
+    console.error("Terjadi kesalahan:", error);
+  } else {
+    const data = response.data;
+    console.log("Data yang diterima:", data);
+    fakultasDataUpdate(data);
+  }
+});
+
+// Untuk ambil nilai dari PRODI ke dropdown
+function prodiDataUpdate(data) {
+  const selectElement = document.getElementById("prodi-update");
+  // Kosongkan isi dropdown saat ini
+  selectElement.innerHTML = "";
+
+  // Loop data yang diterima dari API
+  data.forEach((item) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = item.id_prodi;
+    optionElement.textContent = `${item.prodi} - ${item.id_prodi}`;
+    selectElement.appendChild(optionElement);
+  });
+  selectElement.addEventListener("change", function () {
+    const selectedValue = this.value;
+    // Lakukan sesuatu dengan nilai yang dipilih, misalnya, tampilkan di konsol
+    console.log("Nilai yang dipilih: ", selectedValue);
+  });
+}
+// Panggil API untuk mendapatkan data prodi
+CihuyDataAPI(UrlGetUsersProdi, token, (error, response) => {
+  if (error) {
+    console.error("Terjadi kesalahan:", error);
+  } else {
+    const data = response.data;
+    console.log("Data yang diterima:", data);
+    prodiDataUpdate(data);
+  }
+});
+
+// Untuk ambil nilai dari AUDITOR ke dropdown
+function auditorDataUpdate(data) {
+  const selectElement = document.getElementById("auditor-update");
+  // Kosongkan isi dropdown saat ini
+  selectElement.innerHTML = "";
+
+  // Loop data yang diterima dari API
+  data.forEach((item) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = item.idAuditor;
+    optionElement.textContent = `${item.auditor} - ${item.idAuditor}`;
+    selectElement.appendChild(optionElement);
+  });
+  selectElement.addEventListener("change", function () {
+    const selectedValue = this.value;
+    // Lakukan sesuatu dengan nilai yang dipilih, misalnya, tampilkan di konsol
+    console.log("Nilai yang dipilih: ", selectedValue);
+  });
+}
+// Panggil API untuk mendapatkan data auditor
+CihuyDataAPI(UrlGetUsersAuditor, token, (error, response) => {
+  if (error) {
+    console.error("Terjadi kesalahan:", error);
+  } else {
+    const data = response.data;
+    console.log("Data yang diterima:", data);
+    auditorDataUpdate(data);
+  }
+});
+
+// Untuk ambil nilai dari Anggota 1 ke dropdown
+function anggota1DataUpdate(data) {
+  const selectElement = document.getElementById("anggota1-update");
+  // Kosongkan isi dropdown saat ini
+  selectElement.innerHTML = "";
+
+  // Loop data yang diterima dari API
+  data.forEach((item) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = item.idAuditor;
+    optionElement.textContent = `${item.auditor} - ${item.idAuditor}`;
+    selectElement.appendChild(optionElement);
+  });
+  selectElement.addEventListener("change", function () {
+    const selectedValue = this.value;
+    // Lakukan sesuatu dengan nilai yang dipilih, misalnya, tampilkan di konsol
+    console.log("Nilai yang dipilih: ", selectedValue);
+  });
+}
+// Panggil API untuk mendapatkan data auditor
+CihuyDataAPI(UrlGetUsersAuditor, token, (error, response) => {
+  if (error) {
+    console.error("Terjadi kesalahan:", error);
+  } else {
+    const data = response.data;
+    console.log("Data yang diterima:", data);
+    anggota1DataUpdate(data);
+  }
+});
+
+// Untuk ambil nilai dari Anggota 2 ke dropdown
+function anggota2DataUpdate(data) {
+  const selectElement = document.getElementById("anggota2-update");
+  // Kosongkan isi dropdown saat ini
+  selectElement.innerHTML = "";
+
+  // Loop data yang diterima dari API
+  data.forEach((item) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = item.idAuditor;
+    optionElement.textContent = `${item.auditor} - ${item.idAuditor}`;
+    selectElement.appendChild(optionElement);
+  });
+  selectElement.addEventListener("change", function () {
+    const selectedValue = this.value;
+    // Lakukan sesuatu dengan nilai yang dipilih, misalnya, tampilkan di konsol
+    console.log("Nilai yang dipilih: ", selectedValue);
+  });
+}
+// Panggil API untuk mendapatkan data auditor
+CihuyDataAPI(UrlGetUsersAuditor, token, (error, response) => {
+  if (error) {
+    console.error("Terjadi kesalahan:", error);
+  } else {
+    const data = response.data;
+    console.log("Data yang diterima:", data);
+    anggota2DataUpdate(data);
+  }
+});
+
+// Untuk ambil nilai dari SIKLUS ke dropdown
+function siklusDataUpdate(data) {
+  const selectElement = document.getElementById("siklus-update");
+  // Kosongkan isi dropdown saat ini
+  selectElement.innerHTML = "";
+
+  // Loop data yang diterima dari API
+  data.forEach((item, index) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = index + 1;
+    optionElement.textContent = `Tahun ${item.tahun} - ${index + 1}`;
+    selectElement.appendChild(optionElement);
+  });
+  selectElement.addEventListener("change", function () {
+    const selectedValue = this.value;
+    // Lakukan sesuatu dengan nilai yang dipilih, misalnya, tampilkan di konsol
+    console.log("Nilai yang dipilih: ", selectedValue);
+  });
+}
+// Panggil API untuk mendapatkan data siklus
+CihuyDataAPI(UrlGetSiklus, token, (error, response) => {
+  if (error) {
+    console.error("Terjadi kesalahan:", error);
+  } else {
+    const data = response.data;
+    console.log("Data yang diterima:", data);
+    siklusDataUpdate(data);
+  }
 });
