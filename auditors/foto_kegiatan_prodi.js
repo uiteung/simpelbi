90fd5a9d6d89191e2020420051f9f0619b97e55b
christyuda <chrisyuda@ulbi.ac.id>
2023-10-23 15:40:56 +0700

    feat(getallprosesaudit): kesimpulan,mekanisme,foto

---
+++ auditors/foto_kegiatan_prodi.js
@@ -0,0 +1,78 @@
+import {
+  CihuyDataAPI,
+  //   CihuyPostApi,
+  //   CihuyDeleteAPI,
+  //   CihuyUpdateApi,
+} from "https://c-craftjs.github.io/simpelbi/api.js";
+import {
+  token,
+  //   UrlGetUsersProdi,
+  //   UrlGetUsersFakultas,
+  //   UrlGetJenjang,
+  //   UrlGetSiklus,
+} from "../js/template/template.js";
+import { UrlGetAudit } from "../js/template/template.js";
+function ShowDataAudit(data) {
+  const tableBody = document.getElementById("content");
+  tableBody.innerHTML = "";
+  let nomor = 1;
+
+  data.forEach((item) => {
+    const barisBaru = document.createElement("tr");
+    let statusClass = "";
+    if (item.status === "Sudah Dilaksanakan") {
+      statusClass = "success-button";
+    } else if (item.status === "Belum Dilaksanakan") {
+      statusClass = "custom-button";
+    }
+
+    // Isi kolom-kolom tabel dengan data yang diambil
+    barisBaru.innerHTML = `
+    <td>
+    <div class="userDatatable-content">${nomor}</div>
+  </td>
+      <td>
+          <div class="userDatatable-content">${item.fakultas}</div>
+        </td>
+        <td>
+        <div class="userDatatable-content">${item.prodi}</div>
+      </td>
+      <td>
+      <div class="userDatatable-content">          
+      <img src="https://simbe-dev.ulbi.ac.id/static/pictures/${item.foto}" alt="Foto" width="100" height="100">
+      </div>
+    </td>
+    <td>
+        <div class="userDatatable-content">${item.auditor}</div>
+      </td>
+      <td>
+      <div class="userDatatable-content">${item.tgl}</div>
+    </td>
+      
+        
+      `;
+
+    tableBody.appendChild(barisBaru);
+    nomor++;
+  });
+}
+
+const currentURL = window.location.href;
+const url = new URL(currentURL);
+const id_ami = url.searchParams.get("id_ami");
+
+if (id_ami) {
+  const apiUrl = `https://simbe-dev.ulbi.ac.id/api/v1/foto/get?id_ami=${id_ami}`;
+
+  CihuyDataAPI(apiUrl, token, (error, response) => {
+    if (error) {
+      console.error("Terjadi kesalahan:", error);
+    } else {
+      const data = response.data;
+      console.log("Data yang diterima:", data);
+      ShowDataAudit([data]);
+    }
+  });
+} else {
+  console.log("Parameter id_ami tidak ditemukan dalam URL.");
+}
---
+++ auditors/mekanisme.js
@@ -0,0 +1,74 @@
+import {
+  CihuyDataAPI,
+  //   CihuyPostApi,
+  //   CihuyDeleteAPI,
+  //   CihuyUpdateApi,
+} from "https://c-craftjs.github.io/simpelbi/api.js";
+import {
+  token,
+  //   UrlGetUsersProdi,
+  //   UrlGetUsersFakultas,
+  //   UrlGetJenjang,
+  //   UrlGetSiklus,
+} from "../js/template/template.js";
+function ShowDataMekanisme(data) {
+  const tableBody = document.getElementById("content");
+  tableBody.innerHTML = "";
+  let nomor = 1;
+
+  const pertanyaan = [
+    "Pembukaan dan pertemuan dengan Kaprodi",
+    "Pembukaan dan pertemuan dengan Kaprodi",
+
+    "Pertemuan dengan staf dosen",
+    "Pertemuan dengan karyawan",
+    "Pertemuan dengan mahasiswa",
+    "Pertemuan dengan alumni/pengguna lulusan (jika ada)",
+    "Penyampaian temuan dan penutup",
+  ];
+  data.forEach((item) => {
+    const mekanismeCount = Object.keys(item).filter((key) =>
+      key.startsWith("question")
+    ).length;
+
+    for (let i = 1; i <= mekanismeCount; i++) {
+      const barisBaru = document.createElement("tr");
+      barisBaru.innerHTML = `
+        <td>
+          <div class="userDatatable-content">${nomor}</div>
+        </td>
+        <td>
+        <div class="userDatatable-content">${pertanyaan[i]}</div>
+        </td>
+        <td>
+          <div class="userDatatable-content">${item["question" + i]}</div>
+        </td>
+      `;
+
+      tableBody.appendChild(barisBaru);
+      nomor++;
+    }
+  });
+}
+
+const currentURL = window.location.href;
+const url = new URL(currentURL);
+const id_ami = url.searchParams.get("id_ami");
+
+if (id_ami) {
+  const apiUrl = `https://simbe-dev.ulbi.ac.id/api/v1/mekanisme/getbyami?id_ami=${id_ami}`;
+
+  CihuyDataAPI(apiUrl, token, (error, response) => {
+    if (error) {
+      console.error("Terjadi kesalahan:", error);
+    } else {
+      const data = response.data;
+      console.log("Data yang diterima:", data);
+
+      // Memanggil fungsi ShowDataMekanisme dengan data yang diterima
+      ShowDataMekanisme([data]); // Mengubah data menjadi array tunggal
+    }
+  });
+} else {
+  console.log("Parameter id_ami tidak ditemukan dalam URL.");
+}
