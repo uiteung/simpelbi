628933fd1688eee2a8dc27f2c5f74a6b30d5e081
Valen Rionald <vrionald@gmail.com>
2023-10-23 10:08:33 +0700

    Fix(Prodi): Memperbaiki tampilan dashboard prodi

--- prodi/dashboard-prodi.js
+++ prodi/dashboard-prodi.js
@@ -8,135 +8,44 @@ import {
   UrlGetAudit,
   UrlGetKesimpulan,
 } from "../js/template/template.js";
-function ShowDataProsesAMI(data, mekanismeData, auditData, kesimpulanData) {
+function ShowDataProsesAMI(data) {
   const tableBody = document.getElementById("content");
   tableBody.innerHTML = "";
   let nomor = 1;
 
   data.forEach((item) => {
-    const mekanismeItem = mekanismeData.find(
-      (mekanisme) => mekanisme.idAmi === item.idAmi
-    );
     const barisBaru = document.createElement("tr");
     const kolomNo = document.createElement("td");
-    kolomNo.innerHTML = `<div class="userDatatable-content">${nomor}</div>`;
+    kolomNo.innerHTML = `
+    <div class="userDatatable-content">
+    <table>
+    <tr>
+    <td>
+    ${nomor}</td>
+  </tr>
+    </table>
+    </div>`;
     barisBaru.appendChild(kolomNo);
-    const kolomProsesAudit = document.createElement("td");
-    kolomProsesAudit.innerHTML = `
+    const kolomStatusAmi = document.createElement("td");
+    kolomStatusAmi.innerHTML = `
       <div class="userDatatable-content">
         <table>
-          <tr>
-            <td>Mekanisme</td>
-            <td>
-            <a href="pengawasan-mekanisme.html?id_ami=${item.idAmi}" 
-               style="pointer-events: ${
-                 item.status === "Selesai" ? "none" : "auto"
-               }">
-              ${
-                item.status === "Selesai"
-                  ? '<span class="success-button">Sudah Diisi</span>'
-                  : mekanismeItem
-                  ? mekanismeItem.question1 === "Ya" ||
-                    mekanismeItem.question2 === "Ya" ||
-                    mekanismeItem.question3 === "Ya" ||
-                    mekanismeItem.question4 === "Ya" ||
-                    mekanismeItem.question5 === "Ya" ||
-                    mekanismeItem.question6 === "Ya"
-                    ? '<span class="success-button">Sudah Diisi</span>'
-                    : '<span class="custom-button">Belum Diisi</span>'
-                  : '<span class="custom-button">Belum Diisi</span>'
-              }
-            </a>
-          </td>
-          </tr>
-          <tr>
-            <td>Audit</td>
-            <td>
-            <a href="pengawasan-audit.html?id_ami=${item.idAmi}" 
-            style="pointer-events: ${
-              item.status === "Selesai" ? "none" : "auto"
-            }">
-              ${
-                item.status === "Proses"
-                  ? auditData.some((audit) => audit.id_ami === item.idAmi)
-                    ? '<span class="success-button">Sudah Diisi</span>'
-                    : '<span class="custom-button">Belum Diisi</span>'
-                  : item.status === "Selesai"
-                  ? auditData.some((audit) => audit.id_ami === item.idAmi)
-                    ? '<span class="success-button">Sudah Diisi</span>'
-                    : '<span class="custom-button">Belum Diisi</span>'
-                  : ""
-              }
-            </a>
-          </td>
-          </tr>
-          <tr>
-            <td>Kesimpulan</td>
-            <td>
-            <a href="pengawasan-kesimpulan.html?id_ami=${
-              item.idAmi
-            }" style="pointer-events: ${
-      item.status === "Selesai" ? "none" : "auto"
-    }">
-              ${
-                kesimpulanData.find(
-                  (kesimpulan) => kesimpulan.id_ami === item.idAmi
-                )
-                  ? '<span class="success-button">Sudah Diisi</span>'
-                  : '<span class="custom-button">Belum Diisi</span>'
-              }
-            </a>
-          </td>
-          </tr>
-          <tr>
-            <td>Tanggal RTM</td>
-            <td>
-              ${
-                item.status === "Proses"
-                  ? item.tglRtm
-                    ? `<a href="pengawasan-tanggal_rtm.html?id_ami=${item.idAmi} style="pointer-events" "><span class="success-button">${item.tglRtm}</span></a>`
-                    : `<a href="pengawasan-tanggal_rtm.html?id_ami=${item.idAmi}"><span class="custom-button">Belum Diisi</span></a>`
-                  : item.status === "Selesai"
-                  ? item.tglRtm
-                    ? `<span class="success-button">${item.tglRtm}</span>`
-                    : '<span class="custom-button">Belum Diisi</span>'
-                  : ""
-              }
-            </td>
-          </tr>
-          <tr>
-                <td>Foto Kegiatan</td>
-                <td>
-                    ${
-                      item.status === "Proses"
-                        ? item.foto
-                          ? `<a href="pengawasan-foto-prodi.html?id_ami=${
-                              item.idAmi
-                            }" style="pointer-events: ${
-                              item.status === "Selesai" ? "none" : "auto"
-                            }"><span class="success-button">Sudah Diisi</span></a>`
-                          : `<a href="pengawasan-foto_prodi.html?id_ami=${
-                              item.idAmi
-                            }" style="pointer-events: ${
-                              item.status === "Selesai" ? "none" : "auto"
-                            }"><span class="custom-button">Belum Diisi</span></a>`
-                        : item.status === "Selesai"
-                        ? item.foto
-                          ? `<span class="success-button">Sudah Diisi</span>`
-                          : '<span class="custom-button">Belum Diisi</span>'
-                        : ""
-                    }
-                </td>
-            </tr>
+        <tr>
+        <td ><span class="${
+          item.status === "Selesai" ? "success-button" : "custom-button"
+        }">
+        ${item.status}
+      </span></td>
+      </tr>
         </table>
       </div>`;
-    barisBaru.appendChild(kolomProsesAudit);
+    barisBaru.appendChild(kolomStatusAmi);
     const kolomInformasiAudit = document.createElement("td");
     kolomInformasiAudit.innerHTML = `
       <div class="userDatatable-content">
         <table>
           <tr>
-            <td>Prodi : ${item.prodi}</td>
+            <td">Prodi : ${item.prodi}</td>
           </tr>
           <tr>
             <td>Fakultas : ${item.fakultas}</td>
@@ -151,17 +60,8 @@ function ShowDataProsesAMI(data, mekanismeData, auditData, kesimpulanData) {
             <td>Anggota 2 : ${item.nm_auditor_2}</td>
           </tr>
           <tr>
-            <td>Siklus :    <span class="custom-button">${
-              item.idSiklus
-            } -  Tahun ${item.tahun}</span>
+            <td>Siklus :    <span class="custom-button">${item.idSiklus} -  Tahun ${item.tahun}</span>
             </td>
-          </tr>
-          <tr>
-            <td>Status Akhir : <span class="${
-              item.status === "Selesai" ? "success-button" : "custom-button"
-            }">
-            ${item.status}
-          </span></td>
           </tr>
         </table>
       </div>`;
