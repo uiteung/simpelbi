5a1bed8570c8ac4935fd5130a5e77ff978c33e05
christyuda <chrisyuda@ulbi.ac.id>
2023-10-27 10:45:02 +0700

    feat(searchParams):menyesuaikan dengan  param url

--- auditors/addaudit.js
+++ auditors/addaudit.js
@@ -4,8 +4,9 @@ const addButton = document.getElementById("addbutton");
 // Tambahkan event listener ke tombol
 addButton.addEventListener("click", () => {
   // Ambil id_ami dari URL
-
-  const id_ami = getIdAmiFromURL;
+  const currentURL = window.location.href;
+  const url = new URL(currentURL);
+  const id_ami = url.searchParams.get("id_ami");
 
   // Arahkan pengguna ke halaman "pengawasan-audit-add" dengan menyertakan parameter id_ami
   if (id_ami) {
