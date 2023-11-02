8d076ca9a99f1174c1612d7619bc0dbe8c701fae
christyuda <chrisyuda@ulbi.ac.id>
2023-11-02 14:15:01 +0700

    feat(pagenationcalculate):mencoba membuat fungsi untuk pagenation

--- pengawasan/foto_kegiatan_prodi.js
+++ pengawasan/foto_kegiatan_prodi.js
@@ -14,16 +14,17 @@ import {
 import { UrlGetAudit } from "../js/template/template.js";
 import { populateUserProfile } from "https://c-craftjs.github.io/simpelbi/profile.js";
 
-// Untuk Get Data Profile
-populateUserProfile();
-
-// Untuk Get Data Audit
-function ShowDataAudit(data) {
+// Fungsi untuk menampilkan data audit dengan pagination
+function ShowDataAudit(data, page, itemsPerPage) {
   const tableBody = document.getElementById("content");
   tableBody.innerHTML = "";
-  let nomor = 1;
+  let nomor = (page - 1) * itemsPerPage + 1;
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 
-  data.forEach((item) => {
+  const paginatedData = data.slice(startIndex, endIndex);
+
+  paginatedData.forEach((item) => {
     const barisBaru = document.createElement("tr");
     let statusClass = "";
     if (item.status === "Sudah Dilaksanakan") {
@@ -34,31 +35,49 @@ function ShowDataAudit(data) {
 
     // Isi kolom-kolom tabel dengan data yang diambil
     barisBaru.innerHTML = `
-    <td>
-      <div class="userDatatable-content">${nomor}</div>
-    </td>
-        <td>
-            <div class="userDatatable-content">${item.fakultas}</div>
-          </td>
-          <td>
-          <div class="userDatatable-content">${item.prodi}</div>
-        </td>
-        <td>
-        <div class="userDatatable-content">          
-        <img src="https://simbe-dev.ulbi.ac.id/static/pictures/${item.foto}" alt="Foto" width="100" height="100">
+      <td>
+        <div class="userDatatable-content">${nomor}</div>
+      </td>
+      <td>
+        <div class="userDatatable-content">${item.fakultas}</div>
+      </td>
+      <td>
+        <div class="userDatatable-content">${item.prodi}</div>
+      </td>
+      <td>
+        <div class="userDatatable-content">
+          <img src="https://simbe-dev.ulbi.ac.id/static/pictures/${item.foto}" alt="Foto" width="100" height="100">
         </div>
       </td>
       <td>
-          <div class="userDatatable-content">${item.auditor}</div>
-        </td>
-        <td>
+        <div class="userDatatable-content">${item.auditor}</div>
+      </td>
+      <td>
         <div class="userDatatable-content">${item.tgl}</div>
-      </td>  
-      `;
+      </td>
+    `;
 
     tableBody.appendChild(barisBaru);
     nomor++;
   });
+}
+
+// Fungsi untuk membuat tombol pagination
+function createPaginationButtons(data, itemsPerPage) {
+  const paginationContainer = document.getElementById("pagination");
+  paginationContainer.innerHTML = "";
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  for (let page = 1; page <= totalPages; page++) {
+    const button = document.createElement("button");
+    button.textContent = page;
+    button.addEventListener("click", () => {
+      ShowDataAudit(data, page, itemsPerPage);
+    });
+
+    paginationContainer.appendChild(button);
+  }
 }
 
 const currentURL = window.location.href;
@@ -67,6 +86,7 @@ const id_ami = url.searchParams.get("id_ami");
 
 if (id_ami) {
   const apiUrl = `https://simbe-dev.ulbi.ac.id/api/v1/foto/get?id_ami=${id_ami}`;
+  const itemsPerPage = 10; // Ganti dengan jumlah item per halaman yang diinginkan
 
   CihuyDataAPI(apiUrl, token, (error, response) => {
     if (error) {
@@ -74,7 +94,12 @@ if (id_ami) {
     } else {
       const data = response.data;
       console.log("Data yang diterima:", data);
-      ShowDataAudit(data);
+
+      // Tampilkan data audit pada halaman pertama
+      ShowDataAudit(data, 1, itemsPerPage);
+
+      // Buat tombol-tombol pagination
+      createPaginationButtons(data, itemsPerPage);
     }
   });
 } else {
