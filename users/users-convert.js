06c850789dacaccb1f5a477e7993728d238555ac
christyuda <chrisyuda@ulbi.ac.id>
2024-01-26 12:17:10 +0700

    feat(convert): remove all js in convert

--- users/users-convert.js
+++ users/users-convert.js
@@ -113,109 +113,12 @@ CihuyDataAPI(UrlGetUsersrtm, token, (error, response) => {
     displayPageData(data, currentPage); // siklusdata(data);
   }
 });
-// function ShowDataUsersrtm(data) {
-//   const tableBody = document.getElementById("content");
-
-//   // Kosongkan isi tabel saat ini
-//   tableBody.innerHTML = "";
-//   let nomor = 1;
-
-//   // Loop melalui data yang diterima dari API
-//   data.forEach((item) => {
-//     const barisBaru = document.createElement("tr");
-//     barisBaru.innerHTML = `
-//     <td>
-//        <div class="userDatatable-content">${nomor}</div>
-//     </td>
-//     <td>
-//        <div class="d-flex">
-//           <div class="userDatatable-inline-title">
-//              <a href="#" class="text-dark fw-500">
-//                 <h6>${item.rtm}</h6>
-//              </a>
-//           </div>
-//        </div>
-//     </td>
-//     <td>
-//        <div class="userDatatable-content">
-//           ${item.dekan}
-//        </div>
-//     </td>
-//     <td>
-//        <div class="userDatatable-content">
-//           ${item.nidn}
-//        </div>
-//     </td>
-//     <td>
-//        <div class="userDatatable-content">
-//           ${item.niknip}
-//        </div>
-//     </td>
-//     <td>
-//        <div class="userDatatable-content">
-//           ${item.telp}
-//        </div>
-//     </td>
-//     <td>
-//        <div class="">
-//           ${item.email}
-//        </div>
-//     </td>
-//     <td>
-//          <div class="userDatatable-content">
-//          <img src="https://simbe-dev.ulbi.ac.id/static/pictures/${item.foto}" alt="Foto" width="100" height="100">
-//          </div>
-//       </td>
-//     <td>
-//        <ul class="orderDatatable_actions mb-0 d-flex flex-wrap">
-
-//           <li>
-//              <a href="#" class="edit"  data-target="#new-member-update" data-rtm-id="${item.id_rtm}">
-//                 <i class="uil uil-edit"></i>
-//              </a>
-//           </li>
-//           <li>
-//             <a href="#" class="remove" data-rtm-id="${item.id_rtm}">
-//                <i class="uil uil-trash-alt"></i>
-//             </a>
-//           </li>
-//        </ul>
-//     </td>
-//     `;
-// const removeButton = barisBaru.querySelector(".remove");
-// removeButton.addEventListener("click", () => {
-//   const id_rtm = removeButton.getAttribute("data-rtm-id");
-//   if (id_rtm) {
-//     deletertm(id_rtm);
-//   } else {
-//     console.error("ID rtm tidak ditemukan.");
-//   }
-// });
-// const editButton = barisBaru.querySelector(".edit");
-// editButton.addEventListener("click", () => {
-//   const id_rtm = editButton.getAttribute("data-rtm-id");
-//   if (id_rtm) {
-//     editData(id_rtm);
-//   } else {
-//     console.error("ID rtm tidak ditemukan.");
-//   }
-// });
-
-//     tableBody.appendChild(barisBaru);
-//     nomor++;
-//   });
-// }
 
 function fetchUsernameDataAndPopulateSuggestions() {
   const apiUrlConvert = "https://simbe-dev.ulbi.ac.id/api/v1/convert";
   let dataFromApi = [];
   const usernameInput = document.getElementById("username");
-  const usernameSuggestions = document.getElementById("username-suggestions");
-  //update suggestion
-  const usernameInputUpdate = document.getElementById("username-update");
-  const usernameSuggestionsUpdate = document.getElementById(
-    "username-suggestions-update"
-  );
+
   // Panggil fungsi CihuyDataAPI untuk mengambil data saat halaman dimuat
   CihuyDataAPI(apiUrlConvert, token, (error, response) => {
     if (error) {
