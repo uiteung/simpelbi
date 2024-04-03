d7353e56b01de2f4c2214885a7fa975a4bcadf42
christyuda <chrisyuda@ulbi.ac.id>
2024-04-03 14:28:56 +0700

    feat: tambahkan button kesimpulan js untuk mengambil id_ami dari parameter

---
+++ prodi/button_kesimpulan.js
@@ -0,0 +1,13 @@
+const editButton = document.getElementById("editButton");
+
+editButton.addEventListener("click", function () {
+  const currentURL = window.location.href;
+  const url = new URL(currentURL);
+  const id_ami = url.searchParams.get("id_ami");
+  if (id_ami) {
+    window.location.href = `pengawasan-kesimpulan-edit.html?id_ami=${id_ami}`;
+  } else {
+    // Handle jika id_ami tidak ditemukan dalam URL
+    console.log("Parameter id_ami tidak ditemukan dalam URL.");
+  }
+});
