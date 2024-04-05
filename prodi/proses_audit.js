0d0e08ca504afe66b31afd316b2115f605b89806
christyuda <chrisyuda@ulbi.ac.id>
2024-04-05 14:17:17 +0700

    feat(export): fix untuk print data pengawasan

--- prodi/proses_audit.js
+++ prodi/proses_audit.js
@@ -244,3 +244,224 @@ CihuyDataAPI(UrlGetAuditByAmi, token, (error, response) => {
 //     alert("Parameter 'id_ami' tidak ditemukan dalam URL");
 //   }
 // });
+
+
+
+
+function exportToExcel(data, filename) {
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  XLSX.utils.book_append_sheet(workbook, worksheet, "AMI Data");
+  XLSX.writeFile(workbook, filename);
+}
+
+
+// Function untuk mengekspor data ke CSV
+function exportToCSV(data, filename) {
+  const csv = Papa.unparse(data);
+  const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  const csvURL = window.URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = csvURL;
+  link.setAttribute("download", filename + ".csv");
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+// Function untuk mencetak data
+function printData(data) {
+  
+  let printContent = `
+    <h1>Data Pengawasan Audit </h1>
+    <table border="1">
+      <thead>
+        <tr>
+        <th>
+                              <span class="userDatatable-title">No</span>
+                            </th>
+                            <th>
+                              <span class="userDatatable-title">Standar</span>
+                            </th>
+                            <th>
+                              <span class="userDatatable-title"
+                                >Butir Standar</span
+                              >
+                            </th>
+                            <th>
+                              <span class="userDatatable-title"
+                                >Nama Indikator</span
+                              >
+                            </th>
+                            <th>
+                              <span class="userDatatable-title"
+                                >Isi Indikator</span
+                              >
+                            </th>
+                            <th>
+                              <span class="userDatatable-title"
+                                >Temuan KTS</span
+                              >
+                            </th>
+                            <th>
+                              <span class="userDatatable-title"
+                                >Uraian Ketidak Sesuaian</span
+                              >
+                            </th>
+                            <th>
+                              <span class="userDatatable-title"
+                                >Tindak Perbaikan</span
+                              >
+                            </th>
+                            <th>
+                              <span class="userDatatable-title"
+                                >Target Waktu Perbaikan</span
+                              >
+                            </th>
+                            <th>
+                              <span class="userDatatable-title">Status</span>
+                            </th>
+                            
+  
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+  data.forEach((item, index) => {
+    printContent += `
+      <tr>
+        <td>${index + 1}</td>
+        
+        <td>
+          <div class="d-flex">
+              <div class="userDatatable-inline-title">
+                <a href="#" class="text-dark fw-500">
+                    <h6>${item.standar}</h6>
+                </a>
+              </div>
+          </div>
+        </td>
+        <td>
+          <div class="d-flex">
+              <div class="userDatatable-inline-title">
+                <a href="#" class="text-dark fw-500">
+                    <h6>${item.isi_standar}</h6>
+                </a>
+              </div>
+          </div>
+        </td>
+        <td>
+          <div class="userDatatable-content">
+          ${item.nama_indikator}
+          </div>
+        </td>
+        <td>
+          <div class="userDatatable-content">
+          ${item.isi_indikator}
+          </div>
+        </td>
+        <td>
+          <div class="userDatatable-content" style="font-size: 12px;  white-space: pre-line;">
+            ${item.kts ? item.kts : "belum ada kts"}
+          </div>
+        </td>
+        <td>
+          <div class="userDatatable-content">
+          ${item.uraian}
+          </div>
+        </td>
+        <td>
+          <div class="userDatatable-content">
+          ${item.tindakan}
+          </div>
+        </td>
+        <td>
+        <div class="userDatatable-content">
+        ${item.target}
+        </div>
+
+        <td>
+        <div class="userDatatable-content">
+          <span class=>${item.status}</span>
+        </div>
+      </td>
+      </tr>
+    `;
+  });
+
+  printContent += `
+      </tbody>
+    </table>
+  `;
+
+  const printWindow = window.open("", "_blank");
+  printWindow.document.write(`
+    <html>
+      <head>
+        <title>Pengawasan Audit Prodi - Cetak</title>
+        <style>
+          table {
+            border-collapse: collapse;
+            width: 100%;
+          }
+          th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding:10px;
+          }
+          h1 {
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        ${printContent}
+      </body>
+    </html>
+  `);
+
+  printWindow.document.close();
+  printWindow.print();
+}
+
+function processDataAndExport(exportType, filename) {
+  CihuyDataAPI(UrlGetAuditByAmi, token, (error, response) => {
+    if (error) {
+      console.error("Terjadi kesalahan:", error);
+    } else {
+      const data = response.data;
+      console.log("Data yang diterima:", data);
+
+      // Panggil fungsi sesuai dengan jenis ekspor yang diinginkan
+      switch (exportType) {
+        case "excel":
+          exportToExcel(data, filename + ".xlsx");
+          break;
+        case "csv":
+          exportToCSV(data, filename);
+          break;
+        case "print":
+          printData(data);
+          break;
+        default:
+          console.error("Jenis ekspor tidak valid");
+      }
+    }
+  });
+}
+
+// Panggil fungsi ini ketika tombol Ekspor Excel diklik
+document.getElementById("exportexcel").addEventListener("click", function () {
+  processDataAndExport("excel", "pengawasan_export");
+});
+
+// Panggil fungsi ini ketika tombol Ekspor CSV diklik
+document.getElementById("exportcsv").addEventListener("click", function () {
+  processDataAndExport("csv", "pengawasan_export");
+});
+
+// Panggil fungsi ini ketika tombol Cetak diklik
+document.getElementById("print").addEventListener("click", function () {
+  processDataAndExport("print");
+});
