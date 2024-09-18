5eb64c99f02e12cc21daea887ac7f00349fb0c6a
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-18 15:47:46 +0700

    feat: Add cookie.js for handling login token and redirect logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Burger menus
 document.addEventListener('DOMContentLoaded', function() {
-    // open burger menu
+    // open
     const burger = document.querySelectorAll('.navbar-burger');
     const menu = document.querySelectorAll('.navbar-menu');
 
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // close burger menu
+    // close
     const close = document.querySelectorAll('.navbar-close');
     const backdrop = document.querySelectorAll('.navbar-backdrop');
 
@@ -36,68 +36,5 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
         }
-    }
-
-    // Simpelbi logic
-    const simpelbiCard = document.getElementById("simpelbiCard");
-
-    if (simpelbiCard) {
-        simpelbiCard.addEventListener("click", async (event) => {
-            event.preventDefault();
-
-            const postApiUrlMenu = "https://simbe-dev.ulbi.ac.id/api/v1/menu/";
-
-            try {
-                // Dapatkan token dari cookie yang sudah ada
-                let token = CihuyGetCookie("login");
-
-                // Lakukan permintaan POST
-                const postResult = await CihuyGetHeaders(postApiUrlMenu, token);
-
-                // Parse respons JSON dari permintaan POST
-                const responseData = JSON.parse(postResult);
-
-                // Dapatkan data URL dari respons
-                const dataUrl = responseData.data;
-
-                // Tentukan halaman tujuan berdasarkan data URL
-                let targetPage = "";
-                if (
-                    responseData.code === 400 &&
-                    responseData.success === false &&
-                    responseData.status === "Data user level tidak ditemukan" &&
-                    responseData.data === null
-                ) {
-                    window.location.href = "https://euis.ulbi.ac.id/simpelbi/404.html";
-                } else if (dataUrl === "/admins") {
-                    targetPage = "dashboard-admins.html";
-                } else if (dataUrl === "/prodi") {
-                    targetPage = "dashboard-prodi.html";
-                } else if (dataUrl === "/fakultas") {
-                    targetPage = "dashboard-fakultas.html";
-                } else if (dataUrl === "/auditors") {
-                    targetPage = "dashboard-auditor.html";
-                } else if (
-                    responseData.code === 401 &&
-                    responseData.success === false &&
-                    responseData.status === "Unauthorize Token" &&
-                    responseData.data === null
-                ) {
-                    targetPage = "404.html";
-                } else {
-                    targetPage = "404.html";
-                    console.error("URL tidak sesuai");
-                    return;
-                }
-
-                // Konstruksi URL akhir
-                const finalUrl = `https://euis.ulbi.ac.id/simpelbi${dataUrl}/${targetPage}`;
-
-                // Arahkan pengguna ke URL akhir
-                window.location.href = finalUrl;
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        });
     }
 });
