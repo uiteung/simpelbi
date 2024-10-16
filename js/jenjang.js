cd976fd964b3df46555d7a23d161de564188f747
Syafrial Fachri Pane <syafrial.fachri@ulbi.ac.id>
2024-10-16 10:07:39 +0700

    add import populate profile cihuy js

--- js/jenjang.js
+++ js/jenjang.js
@@ -1,19 +1,23 @@
 import { ShowdataJenjang } from "../js/config/configjenjang.js";
-import { UrlGetJenjang, UrlPostJenjang, token } from "../js/template/template.js";
-import { CihuyPostKTS } from "./config/configkts.js";
 import {
-    CihuyDataAPI,
-  } from "https://c-craftjs.github.io/simpelbi/api.js";
+  UrlGetJenjang,
+  UrlPostJenjang,
+  token,
+} from "../js/template/template.js";
+import { CihuyPostKTS } from "./config/configkts.js";
+import { CihuyDataAPI } from "https://c-craftjs.github.io/simpelbi/api.js";
+import { populateUserProfile } from "https://c-craftjs.github.io/simpelbi/profile.js";
+populateUserProfile();
 
-  CihuyDataAPI(UrlGetJenjang, token, (error, response) => {
-    if (error) {
-      console.error("Terjadi kesalahan:", error);
-    } else {
-      const data = response.data;
-      console.log("Data yang diterima:", data);
-      ShowdataJenjang(data);
-    }
-  });
+CihuyDataAPI(UrlGetJenjang, token, (error, response) => {
+  if (error) {
+    console.error("Terjadi kesalahan:", error);
+  } else {
+    const data = response.data;
+    console.log("Data yang diterima:", data);
+    ShowdataJenjang(data);
+  }
+});
 
 const Tombol = document.getElementById("insbutton");
 
@@ -22,21 +26,22 @@ Tombol.addEventListener("click", async function (e) {
   console.log("Button Clicked");
 
   const jenjangval = document.getElementById("jenjang").value;
-  
+
   var raw = JSON.stringify({
-    "jenjang": jenjangval
+    jenjang: jenjangval,
   });
 
   // Mengirim permintaan POST menggunakan fungsi CihuyPostApi
   CihuyPostKTS(UrlPostJenjang, token, raw)
-  .then((responseText) => {
-    console.log("Response:", responseText);
-    window.alert("Data berhasil ditambahkan!");
-    window.location.replace("https://euis.ulbi.ac.id/simpelbi/settings/data_jenjang.html")
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-    // Tangani kesalahan jika terjadi
-  });
-
+    .then((responseText) => {
+      console.log("Response:", responseText);
+      window.alert("Data berhasil ditambahkan!");
+      window.location.replace(
+        "https://euis.ulbi.ac.id/simpelbi/settings/data_jenjang.html"
+      );
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Tangani kesalahan jika terjadi
+    });
 });
